e729b30c3ef2a540fdbfab070f8ee3d75566e496
Joshua Zimmerman <jzimms@fb.com>
2022-06-24 06:39:31 -0400

    initial commit

--- src/my-first-app/index.js
+++ src/my-first-app/index.js
@@ -3,12 +3,12 @@ import Kviews from './kviews';
 // When trying to install or use this template, make sure to postfix the `app` property below with a unique value.
 // For example, my_first_app__myKustomerOrg01_01
 export default {
-    app: 'my_first_app',
+    app: 'app_expert_testing',
     version: '0.0.1',
     appDetails: { // note: the object and all it's fields are optional, but will be used to fill out the app store listing within Kustomer
         appDeveloper: {
-            name: '<<Insert the developer\'s name>>',
-            website: '<<Insert the developer\'s website>>',
+            name: 'JZimms',
+            website: 'In progress',
             supportEmail: 'example@email.com',
         },
         externalPlatform: {
@@ -20,7 +20,7 @@ export default {
             url: 'www.example.com',
         }],
     },
-    title: 'My First App',
+    title: 'App Expert Testing',
     description: 'This app is just a small example of what an app can do!',
     iconUrl: 'https://pbs.twimg.com/profile_images/1146053597785993217/DcPQyO9Q_400x400.png',
     postInstallMessage: 'Congratulations! You\'ve installed your first successful app!',
